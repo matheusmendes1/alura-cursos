293eea8dd9723eb834c37a87fea1bed29d4ae571
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-01-12 19:19:58 -0300

    End Chapter 6 - Part 1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,13 +11,24 @@ class NegociacaoController{
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        
+        this._mensagemView = new MensagemView();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event){
         
         event.preventDefault();
+
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociacão adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();   
     }
     
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View{
     
-    constructor(elemento) {
-        
-        this._elemento = elemento;
+    constructor(elemento){
+        super(elemento);
     }
-    
-    _template(model) {
+
+    template(model) {
         
         return `
         <table class="table table-hover table-bordered">
@@ -41,9 +40,5 @@ class NegociacoesView {
         </table>
         `;
     }
-    
-    update(model) {
-        
-        this._elemento.innerHTML = this._template(model);
-    }
+
 }
