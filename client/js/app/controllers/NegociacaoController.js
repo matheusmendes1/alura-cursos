d6e276a5cc3b7ba352171409589e9bfd94b4ed65
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-01-16 16:42:17 -0300

    A commit

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,8 +7,11 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        
+
+        /*this._listaNegociacoes = 
+            new ListaNegociacoes(model => this._negociacoesView.update(model));
+        */
+       
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
@@ -24,12 +27,19 @@ class NegociacaoController{
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociacão adicionada com sucesso!';
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();   
+    }
+
+    apaga() {
+
+        this._listaNegociacoes.esvazia();
+    
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
     
     _criaNegociacao(){
