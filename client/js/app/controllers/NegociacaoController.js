a7fd8c079f7f06009649acb293f5b2697a78c5c6
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-01-17 19:08:38 -0300

    another commit

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -28,18 +28,21 @@ class NegociacaoController{
     importaNegociacoes() {
 
         let service = new NegociacaoService();
-      
-        service.obterNegociacoesDaSemana((erro,negociacoes) => {
-            if(erro) {
-                this._mensagem.texto = erro;
-                return;
-            }
-      
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+    
+        Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes => {
+            negociacoes
+              .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+              .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
             this._mensagem.texto = 'Negociações importadas com sucesso';
-        });
-      }
-      
+        })
+        .catch(erro => this._mensagem.texto = erro);  
+    
+    }
+
     apaga() {
 
         this._listaNegociacoes.esvazia();
